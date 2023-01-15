8c4404acfe2040fbcd4413e3122a587bb52d62f5
fluorjo <fluorjo@naver.com>
2023-01-15 18:56:26 +0900

    페이지 넘기기 기능

--- lib/screens/screens_search.dart
+++ lib/screens/screens_search.dart
@@ -12,12 +12,25 @@ class _SearchScreenState extends State<SearchScreen> {
   String result = '';
   List? data;
   TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
     data = List.empty(growable: true);
     _editingController = TextEditingController();
+    _scrollController = ScrollController();
+
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print('bottom');
+        page++;
+        getJSONData();
+      }
+    });
   }
 
   @override
@@ -80,11 +93,15 @@ class _SearchScreenState extends State<SearchScreen> {
                       ),
                     );
                   },
-                  itemCount: data!.length),
+                  itemCount: data!.length,
+                  controller: _scrollController,
+                ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          page = 1;
+          data!.clear();
           getJSONData();
         },
         child: const Icon(Icons.file_download),
@@ -94,7 +111,7 @@ class _SearchScreenState extends State<SearchScreen> {
 
   Future<String> getJSONData() async {
     var url =
-        'https://dapi.kakao.com/v3/search/book?target=title&query=${_editingController!.value.text}';
+        'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController!.value.text}';
 
     var response = await http.get(Uri.parse(url),
         headers: {"Authorization": "KakaoAK cc88f89bd0a8b58cce4d732fac88b0e8"});
