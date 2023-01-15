d6c2cc04c463a7fa1ec0f2079017056a41e063e1
fluorjo <fluorjo@naver.com>
2023-01-15 15:05:48 +0900

    책 목록 불러오기 완성

--- lib/screens/screens_search.dart
+++ lib/screens/screens_search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -8,23 +9,71 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   String result = '';
+  List? data;
+
+  @override
+  void initState() {
+    super.initState();
+    data = List.empty(growable: true);
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('http example'),
       ),
       body: Container(
         child: Center(
-          child: Text(result),
+          child: data!.isEmpty
+              ? const Text(
+                  '데이터가 없습니다.',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                )
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(data![index]['title'].toString()),
+                            Text(data![index]['authors'].toString()),
+                            Text(data![index]['sale_price'].toString()),
+                            Text(data![index]['status'].toString()),
+                            Image.network(
+                              data![index]['thumbnail'],
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.contain,
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: data!.length),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          getJSONData();
+        },
         child: const Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK cc88f89bd0a8b58cce4d732fac88b0e8"});
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data!.addAll(result);
+    });
+
+    return response.body;
   }
 }
