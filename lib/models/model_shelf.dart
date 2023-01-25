7eae803ed77e0877ce9070c00bda7e822efb1f87
fluorjo <fluorjo@naver.com>
2023-01-25 23:26:07 +0900

    (다시)책장 책 리스트 불러오기

--- lib/models/model_shelf.dart
+++ lib/models/model_shelf.dart
@@ -1,86 +1,81 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:book_report_fluor/models/model_book.dart';
 
 class ShelfProvider with ChangeNotifier {
+  late CollectionReference userReference;
   late CollectionReference shelfReference;
-  List<Book> bookShelf = [];
+  List<Book> booksInShelf = [];
 
   ShelfProvider({reference}) {
-    shelfReference =
-        reference ?? FirebaseFirestore.instance.collection('User');
+    userReference = reference ?? FirebaseFirestore.instance.collection('User');
   }
 
   Future<void> fetchBookOrCreateShelf(String uid) async {
     if (uid == '') {
       return;
     }
-    final shelfReferenceUserID = await shelfReference.doc(uid).get();
-
-    print('shelfReferenceUserID');
-    print(shelfReferenceUserID);
-    print('shelfReferenceUserID.data()');
-    print(shelfReferenceUserID.data());
-    print('shelfReferenceUserID.exists');
-    print(shelfReferenceUserID.exists);
-
-    // final allData = userBookshelf.docs.map((doc) => doc.data()).toList();
-    // print('userBookshelf');
-    // print(userBookshelf);
-    // print(userBookshelf.runtimeType);
-
-    // print('allData');
-    // print(allData);
-    // print(allData.runtimeType);
+    final shelfReferenceUserID = await userReference.doc(uid).get();
+    shelfReference = userReference.doc(uid).collection('myBookShelf');
+    final UserBookShelfJQSnapshot =
+        await userReference.doc(uid).collection('myBookShelf').get();
+    final BookListOfShelf =
+        UserBookShelfJQSnapshot.docs.map((doc) => doc.data()).toList();
+    print(BookListOfShelf);
+    print(BookListOfShelf.runtimeType);
     if (shelfReferenceUserID.exists) {
       Map<String, dynamic> bookShelfMap =
           shelfReferenceUserID.data() as Map<String, dynamic>;
       print('-------bookShelfMap--------');
       print(bookShelfMap);
       print(bookShelfMap.runtimeType);
-      List<Book> temp = [];
-      for (var book in bookShelfMap['books']) {
-        temp.add(Book.fromMap(book));
-      }
-      bookShelf = temp;
-      notifyListeners();
-    } else {
-      await shelfReference.doc(uid).collection('myBookShelf')
-      .doc('temp')
-      .set({'temp': []});
-      
-      await shelfReference.doc(uid)
-      .set({'tempDoc': []});
+
+      print('-------shelfReference--------');
+      print(shelfReference);
+      // List<Book> temp = [];
+      // for (var book in bookShelfMap['books']) {
+      //   temp.add(Book.fromMap(book));
+      // }
+      // bookShelf = temp;
 
       notifyListeners();
-      // await shelfReference.doc(uid).collection('myBookShelf').doc('dwe').delete();
+    } else {
+      await userReference
+          .doc(uid)
+          .collection('myBookShelf')
+          .doc('temp')
+          .set({'temp': []});
+
+      await userReference.doc(uid).set({'tempDoc': []});
+
+      notifyListeners();
+      // await userReference.doc(uid).collection('myBookShelf').doc('dwe').delete();
     }
   }
 
-  Future<void> addBookToShelf(String uid, Book book) async {
-    bookShelf.add(book);
-    Map<String, dynamic> bookShelfMap = {
-      'books': bookShelf.map((book) {
-        return book.toSnapshot();
-      }).toList()
-    };
-    await shelfReference.doc(uid).set(bookShelfMap);
-    notifyListeners();
-  }
+  // Future<void> addBookToShelf(String uid, Book book) async {
+  //   bookShelf.add(book);
+  //   Map<String, dynamic> bookShelfMap = {
+  //     'books': bookShelf.map((book) {
+  //       return book.toSnapshot();
+  //     }).toList()
+  //   };
+  //   await userReference.doc(uid).set(bookShelfMap);
+  //   notifyListeners();
+  // }
 
-  Future<void> removeBookFromShelf(User? user, Book book) async {
-    bookShelf.removeWhere((element) => element.id == book.id);
-    Map<String, dynamic> bookShelfMap = {
-      'books': bookShelf.map((book) {
-        return book.toSnapshot();
-      }).toList()
-    };
-    await shelfReference.doc(user!.uid).set(bookShelfMap);
-    notifyListeners();
-  }
+  // Future<void> removeBookFromShelf(User? user, Book book) async {
+  //   bookShelf.removeWhere((element) => element.id == book.id);
+  //   Map<String, dynamic> bookShelfMap = {
+  //     'books': bookShelf.map((book) {
+  //       return book.toSnapshot();
+  //     }).toList()
+  //   };
+  //   await userReference.doc(user!.uid).set(bookShelfMap);
+  //   notifyListeners();
+  // }
 
   bool isBookInShelf(Book book) {
-    return bookShelf.any((element) => element.id == book.id);
+    return booksInShelf.any((element) => element.id == book.id);
   }
 }
--- lib/tabs/tab_shelf.dart
+++ lib/tabs/tab_shelf.dart
@@ -1,11 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:book_report_fluor/models/model_shelf.dart';
 
 class ShelfTab extends StatelessWidget {
+  late String uid = '';
+
+  Future<void> getUid() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    uid = prefs.getString('uid') ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:Text('서재')
-    );
+    final shelfProvider = Provider.of<ShelfProvider>(context);
+    getUid();
+
+    // return FutureBuilder(
+    //   future: shelfProvider.fetchBookOrCreateShelf(uid),
+    //   builder: (context, snapshot) {
+    //     if(shelfProvider.b)
+        
+    //   },snapshot
+    //   )
+    
+    return const Center(child: Text('서재'));
   }
 }
